4937ce6d94d5f7ddb60d900c9511ab76fb9d5711
sharavanan-code <sharavanan045@gmail.com>
2025-04-07 07:28:07 +0530

    frist_commit

--- remove_dulicate.java
+++ remove_dulicate.java
@@ -1,10 +1,18 @@
 public class remove_dulicate {
     public static void main(String[] args) {
-        String str ="hello";
+        String str ="hello my name is shararvanan how are you?";
+        String spaces = "";
         String result ="";
 
-        for(int i =0;i<str.length();i++){
-            char current_char = str.charAt(i);
+        for(int i = 0;i<str.length();i++){
+            char remove_space = str.charAt(i);
+            if(remove_space!=' '){
+                spaces+=str.charAt(i);
+            }
+        }
+
+        for(int i =0;i<spaces.length();i++){
+            char current_char = spaces.charAt(i);
             
             if(result.indexOf(current_char)==-1){
                 result+=current_char;
