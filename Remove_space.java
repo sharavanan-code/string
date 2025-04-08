84f7e5166984368ed4f9d0343d8906397ce4969b
sharavanan-code <sharavanan045@gmail.com>
2025-04-08 09:00:30 +0530

    frist_commit

--- Remove_space.java
+++ Remove_space.java
@@ -1,6 +1,6 @@
 public class Remove_space {
     public static void main(String[] args) {
-        String str = "hello hi";
+        String str = "hello hi then";
         String result ="";
         for(int i = 0;i<=str.length()-1;i++){
             char ch = str.charAt(i);
